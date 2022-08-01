641057bd560f149aeadfe3fe4a28e4aa030ebda2
Steve Morello <smorello4@gmail.com>
2022-08-01 11:24:03 -0400

    get restaurant by id successful

--- server.js
+++ server.js
@@ -10,14 +10,23 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
+// ROUTES
+//get all restaurants
 app.get('/restaurants', async (req, res) => {
   let restaurants = await Restaurant.find()
   res.send({ restaurants })
 })
 
+//create new restaurant
 app.post('/restaurants', async (req, res) => {
   let newRestaurant = await Restaurant.create(req.body)
   res.send(newRestaurant)
+})
+
+//get one restaurant
+app.get('/restaurants/:id', async (req, res) => {
+  let restaurantById = await Restaurant.findById(req.params.id)
+  res.send({ restaurantById })
 })
 
 app.get('/*', (req, res) => {
