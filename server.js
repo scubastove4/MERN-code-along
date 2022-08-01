fd605c6442119cf510de9a598639e4d853da7e47
Steve Morello <smorello4@gmail.com>
2022-08-01 09:49:21 -0400

    set up Heroku

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.static(`${__dirname}/client/build`))
+
 app.get('/', (req, res) => {
   res.send('I am root')
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 app.listen(PORT, () => {
