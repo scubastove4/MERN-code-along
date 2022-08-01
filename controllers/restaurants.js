43120797bcb94139c1eb0395ebfa57a809327a4f
Steve Morello <smorello4@gmail.com>
2022-08-01 12:13:18 -0400

    refactor controllers to be more modular

---
+++ controllers/restaurants.js
@@ -0,0 +1,25 @@
+const { Restaurant } = require('../models')
+
+//get all restaurants
+const getAllRestaurants = async (req, res) => {
+  let restaurants = await Restaurant.find()
+  res.send({ restaurants })
+}
+
+//create new restaurant
+const createRestaurant = async (req, res) => {
+  let newRestaurant = await Restaurant.create(req.body)
+  res.send(newRestaurant)
+}
+
+//get one restaurant
+const getRestaurantById = async (req, res) => {
+  let restaurantById = await Restaurant.findById(req.params.id)
+  res.send({ restaurantById })
+}
+
+module.exports = {
+  getAllRestaurants,
+  createRestaurant,
+  getRestaurantById
+}
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const { Router } = require('express')
 const router = Router()
-const controllers = require('../controllers')
+const controllers = require('../controllers') //could also destructure restaurants from controllers
 
 //get all restaurants
-router.get('/restaurants', controllers.getAllRestaurants)
+router.get('/restaurants', controllers.restaurants.getAllRestaurants)
 
 //create new restaurant
-router.post('/restaurants', controllers.createRestaurant)
+router.post('/restaurants', controllers.restaurants.createRestaurant)
 
 //get one restaurant
-router.get('/restaurants/:id', controllers.getRestaurantById)
+router.get('/restaurants/:id', controllers.restaurants.getRestaurantById)
 
 module.exports = router
