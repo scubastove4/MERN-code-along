2567d981314e58288ecf4aadd512f9473159e565
Steve Morello <smorello4@gmail.com>
2022-08-01 11:16:28 -0400

    /restaurants post req successful

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ const app = express()
 
 const { Restaurant } = require('./models')
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/restaurants', async (req, res) => {
-  const restaurants = await Restaurant.find()
+  let restaurants = await Restaurant.find()
   res.send({ restaurants })
+})
+
+app.post('/restaurants', async (req, res) => {
+  let newRestaurant = await Restaurant.create(req.body)
+  res.send(newRestaurant)
 })
 
 app.get('/*', (req, res) => {
