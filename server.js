358ca200b4fdfc70fed32fa7e6079332d1900c74
Steve Morello <smorello4@gmail.com>
2022-08-01 15:30:36 -0400

    add db on in server.js. install cors and morgan

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+
 const PORT = process.env.PORT || 3001
 const db = require('./db')
 const routes = require('./routes')
@@ -6,12 +9,16 @@ const routes = require('./routes')
 const app = express()
 
 //MIDDLEWARE
+app.use(cors())
+app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
 // ROUTES
 app.use('/api', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
