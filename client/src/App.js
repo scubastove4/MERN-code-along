18769efde6e9f87b5d3b59cd3ff6c56c051b8ff9
Steve Morello <smorello4@gmail.com>
2022-08-02 10:28:12 -0400

    set up react app, scaffold navbar

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <nav>This is nav</nav>
+}
+
+export default NavBar
