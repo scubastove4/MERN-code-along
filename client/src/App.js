6e3da7a95537a4d27b1958ca78f971fc874007c2
Steve Morello <smorello4@gmail.com>
2022-08-02 11:27:36 -0400

    successful axios.get all restaurants on home

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import NavBar from './components/NavBar'
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Home from './pages/Home'
 import RestaurantList from './pages/RestaurantList'
@@ -7,10 +9,22 @@ import RestaurantDetails from './pages/RestaurantDetails'
 import RestaurantForm from './pages/RestaurantForm'
 import ReviewDetails from './pages/ReviewDetails'
 import ReviewForm from './pages/ReviewForm'
+import { BASE_URL } from './globals'
 
 import './App.css'
 
 const App = () => {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    const getRestaurants = async () => {
+      let res = await axios.get(`${BASE_URL}/api/restaurants`)
+      // console.log(res.data.restaurants)
+      setRestaurants(res.data.restaurants)
+    }
+    getRestaurants()
+  }, [])
+
   return (
     <div>
       <header>
